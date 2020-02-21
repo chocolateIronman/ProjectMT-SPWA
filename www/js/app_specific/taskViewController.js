f4951aefb174fb1cefde90a03228d6254a614ce5
chocolateIronman <44294823+chocolateIronman@users.noreply.github.com>
2020-02-21 16:32:11 +0000

    added comments

--- www/js/app_specific/taskViewController.js
+++ www/js/app_specific/taskViewController.js
@@ -29,16 +29,16 @@
                 }
             })
 
-          
+          //get the ID of the task from the passed state parameters
             var params = $stateParams;
             console.log("PARAMS",params);
-
+            //get the task based on the ID
             tasksSrvc.getTask(params.selected).then(
                 function successCallback(response) {
                     console.log(response.data[0]);
 
                     vm.task = response.data[0];
-                    vm.task.StartDate=moment(parseInt(response.data[0].StartDate)).format("DD/MM/YYYY");
+                    vm.task.StartDate=moment(parseInt(response.data[0].StartDate)).format("DD/MM/YYYY");//convert the date from epoch time to human readable
                     vm.task.DueDate=moment(parseInt(response.data[0].DueDate)).format("DD/MM/YYYY");
                     vm.task.EndDate=moment(parseInt(response.data[0].EndDate)).format("DD/MM/YYYY");
                 },
