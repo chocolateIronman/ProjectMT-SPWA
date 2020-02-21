e6db5fb8f65db3573b11b09375bd164b36991b77
chocolateIronman <44294823+chocolateIronman@users.noreply.github.com>
2020-02-21 16:27:46 +0000

    added comments

--- www/js/app_specific/taskCreateController.js
+++ www/js/app_specific/taskCreateController.js
@@ -26,22 +26,22 @@
                 startDateHolder: Date(),
                 dueDateHolder: Date()
             });
-
+            //get the ID of the selected existing project from the state parameters and add it to the new task object
             var projectid=$stateParams.projectID;
-            vm.newTask.projectID=projectid;
+            vm.newTask.projectID=projectid; //the task is allocated to the specific project
             console.log($stateParams);
             console.log("ProjectID = " +projectid);
-            vm.newTask.EndDate = 0;
+            vm.newTask.EndDate = 0; //set the end date of the project to 0
             
-
+            //create and save the new task
             vm.saveTask = function () {
                 console.log("SAVING TASK!!!");
-                vm.newTask.StartDate=moment(vm.startDateHolder,"DD/MM/YYYY").valueOf();
-                vm.newTask.DueDate=moment(vm.dueDateHolder,"DD/MM/YYYY").valueOf();
+                vm.newTask.StartDate=moment(vm.startDateHolder,"DD/MM/YYYY").valueOf(); //get the date and convert it ot epoch time
+                vm.newTask.DueDate=moment(vm.dueDateHolder,"DD/MM/YYYY").valueOf();//get the date and convert it to epoch time
                 console.table(vm.newTask);
                 //TODO: Error Handling
                 tasksSrvc.createTask(vm.newTask).then(function(){
-                    $state.go('tasksList',{projectID: projectid});
+                    $state.go('tasksList',{projectID: projectid}); //if task is saved successfully go to its details view
                 });
             };
         }
